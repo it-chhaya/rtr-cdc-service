a28e3742dd5caa37393e9658f9508c252999f235
techmentors2024 <techmentors2024@gmail.com>
2025-09-17 17:52:03 +0700

    init first

---
+++ src/test/java/co/istad/cdc/CdcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
