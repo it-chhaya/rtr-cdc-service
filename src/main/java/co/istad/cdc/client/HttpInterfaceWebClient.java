afe0bf8a57fd17a773ec6ad78525c5d61a35398c
techmentors2024 <techmentors2024@gmail.com>
2025-10-10 17:59:59 +0700

    add oauth2 client

--- src/main/java/co/istad/cdc/client/HttpInterfaceWebClient.java
+++ src/main/java/co/istad/cdc/client/HttpInterfaceWebClient.java
@@ -1,6 +1,8 @@
 package co.istad.cdc.client;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
@@ -11,11 +13,17 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 public class HttpInterfaceWebClient {
 
     private final WebClient.Builder loadBalancedWebClientBuilder;
+    private final OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager;
 
     public <T> T createClient(String baseUrl, Class<T> interfaceClass) {
+
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(oAuth2AuthorizedClientManager);
+        oauth2.setDefaultClientRegistrationId("internal");
+
         // Step 1 => Create web client object
         WebClient webClient = loadBalancedWebClientBuilder
                 .baseUrl(baseUrl)
+                .apply(oauth2.oauth2Configuration())
                 .build();
 
         return createClient(webClient, interfaceClass);
